12ac5ad452a1ca21a8e07ba44de4612ad5704311
Gabriel Simon <gabrielsimon775@gmail.com>
2021-08-19 19:58:09 -0300

    Implementação das telas Visitantes, Registro e Relatório de Visitas e seus respectivos campos

---
+++ WebApplication3/Registro.aspx
@@ -0,0 +1,61 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Registro.aspx.cs" Inherits="WebApplication3.Registro" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="telaRegistro" runat="server">
+        <div>
+			<table>
+				<tr>
+					<td>
+						<asp:Label ID="LabelPlaca" runat="server" Text="Placa:"></asp:Label>
+					</td>
+					<td>
+						<asp:TextBox ID="txtPlaca" runat="server"></asp:TextBox>
+					&nbsp;<asp:Button ID="btnPesquisar" runat="server" Text="PESQUISAR" />
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<asp:Label ID="LabelNomeRegistro" runat="server" Text="Nome:"></asp:Label>
+					</td>
+					<td>
+						<asp:TextBox ID="txtNomeRegistro" runat="server"></asp:TextBox>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<asp:Label ID="LabelDataRegistro" runat="server" Text="Data:"></asp:Label>
+					</td>
+					<td>
+						<asp:TextBox ID="txtDataRegistro" runat="server"></asp:TextBox>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<asp:Label ID="LabelHora" runat="server" Text="Hora:"></asp:Label>
+					</td>
+					<td>
+						<asp:TextBox ID="txtHoraRegistro" runat="server"></asp:TextBox>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<asp:Label ID="LabelApto" runat="server" Text="APTO N°:"></asp:Label>
+					</td>
+					<td>
+						<asp:TextBox ID="txtApto" runat="server"></asp:TextBox>
+					</td>
+				</tr>
+			</table>
+			<asp:Button ID="btnConfirmarRegistro" runat="server" Text="CONFIRMAR" />
+			<asp:Button ID="btnCancelarRegistro" runat="server" Text="CANCELAR" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication3/Registro.aspx.designer.cs
@@ -0,0 +1,143 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplication3
+{
+
+
+	public partial class Registro
+	{
+
+		/// <summary>
+		/// Controle telaRegistro.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.HtmlControls.HtmlForm telaRegistro;
+
+		/// <summary>
+		/// Controle LabelPlaca.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label LabelPlaca;
+
+		/// <summary>
+		/// Controle txtPlaca.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox txtPlaca;
+
+		/// <summary>
+		/// Controle btnPesquisar.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Button btnPesquisar;
+
+		/// <summary>
+		/// Controle LabelNomeRegistro.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label LabelNomeRegistro;
+
+		/// <summary>
+		/// Controle txtNomeRegistro.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox txtNomeRegistro;
+
+		/// <summary>
+		/// Controle LabelDataRegistro.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label LabelDataRegistro;
+
+		/// <summary>
+		/// Controle txtDataRegistro.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox txtDataRegistro;
+
+		/// <summary>
+		/// Controle LabelHora.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label LabelHora;
+
+		/// <summary>
+		/// Controle txtHoraRegistro.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox txtHoraRegistro;
+
+		/// <summary>
+		/// Controle LabelApto.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Label LabelApto;
+
+		/// <summary>
+		/// Controle txtApto.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.TextBox txtApto;
+
+		/// <summary>
+		/// Controle btnConfirmarRegistro.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Button btnConfirmarRegistro;
+
+		/// <summary>
+		/// Controle btnCancelarRegistro.
+		/// </summary>
+		/// <remarks>
+		/// Campo gerado automaticamente.
+		/// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+		/// </remarks>
+		protected global::System.Web.UI.WebControls.Button btnCancelarRegistro;
+	}
+}
